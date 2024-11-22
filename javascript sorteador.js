7de12af57b7c7286cd86d092b0b643576a4a3627
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2024-11-22 08:42:36 -0300

    Update javascript sorteador.js

--- javascript sorteador.js
+++ javascript sorteador.js
@@ -25,3 +25,21 @@ function selecColors(quantidade, vetorCores){
     
     return vetorSelecionados
 }
+
+=========
+
+function selecColors(quantidade, vetorCores){
+    let vetorSelecionados = []
+    
+    if(quantidade > vetorCores.length){
+        alert("Você não pode pedir uma quantidade de cores maior do que existe no vetor")return;
+    }
+    
+    for(let i - 0; i < quantidade; i++){
+        let i_aleatório - Math.floor(Math.random()*vetorCores.length)
+        let cor_selecionada = vetorCores.splice(i_aleatório, 1)
+        vetorSelecionados = [...vetorSelecionados, ...cor_selecionada]
+    }
+    
+    return vetorSelecionados
+}
