307442159798a97432bdf610767fb047c57c1af8
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2025-10-06 18:07:42 -0300

    Create BD Exercicios.sql

---
+++ BD2/BD Exercicios.sql
@@ -0,0 +1,44 @@
+/*a)	Use a tabela conta a pagar para gerar um relatorio com o seguinte layout
+ 
+
+Obs: Os dia em atraso devem ser calculados somente para fatura NÃO PAGAS e que estejam com o pagamento em atraso. Caso contrario, o campo atraso deve ser zero.
+
+b)	Use a tabela conta a receber para gerar um relatorio que mostre:
+a.	O nome da empresa
+b.	Quantidade de fatura ainda não pagar 
+c.	Valor total ainda não pago
+ 
+
+c)	Gerar uma lista com o nome  e o total a pagar de todas as empresa que estão na tabela contas a pagar e NÃO estão na tabela do contas a receber.
+
+d)	Usando a mesma tabela, gere um relatorio que mostre
+a.	O nome da empresa
+b.	Quantidade de fatura ainda não pagar 
+c.	Valor total ainda não pago
+d.	Quantidade de fatura já pagas
+e.	Valor ds faturas já pagas
+
+ 
+
+e)	Use as tabelas pagar e receber para gerar um  fluxo de caixa que mostre:
+	Nome da empresa,
+	Idfatura
+	Descricao,
+	Valor do debito (para faturas do contas a pagar)
+	Valor do credito (para fatura do contas a receber)
+	Vencimento
+
+Obs:  Mostrar somente as fatura AINDA NÃO PAGAS (campo pagamento sem data).
+Deve ser mostrado em ordem de data
+
+ 
+f)	Totalizar o fluxo de caixa por dia, mostrando a data, dia da semana, total a pagar e total a receber.
+ 
+
+g)	Usar o mesmo sql acima e mostre somente os dias em que o total a pagar seja maior que R$ 1.000,00
+
+
+
+
+
+
