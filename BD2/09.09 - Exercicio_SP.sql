48e5aa798cb9e55acd6db394c2c9c34189990c58
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2025-10-06 21:20:54 -0300

    Update 09.09 - Exercicio_SP.sql

--- BD2/09.09 - Exercicio_SP.sql
+++ BD2/09.09 - Exercicio_SP.sql
@@ -7,20 +7,20 @@ create proc sp_iu_colabolador
    @col_dtnascimento datetime)
 as
 
-if @Idcolaborador is null
+if @Idcolaborador is null /*se o idcolaborador for nulo, logo o user não foi forneceu o id. nesse caso, o banco vai inserir os valores.*/
    insert colaborador 
       (col_nome,
        col_dtnascimento)
    values 
 	  (@col_nome,
        @col_dtnascimento)
-else
+else 
    begin
-      if exists (select idcolaborador from colaborador where idcolaborador=@Idcolaborador)
+      if exists (select idcolaborador from colaborador where idcolaborador=@Idcolaborador) /*se o idcolaborador NÃO for nulo, o user forneceu um id. Nesse caso, o procedure vai verificar se o idcolaborador ja existe. Se NÃO existir, vai informar que o id não existe, e se existir, ele realizará o update. */
          update colaborador set
             col_nome		= @col_nome,
-            col_dtnascimento= @col_dtnascimento
-         where idcolabroador=@Idcolaborador
+            col_dtnascimento = @col_dtnascimento
+         where Idcolaborador = @Idcolaborador
       else
          select 'O colaborador que você deseja alterar os dados NÃO EXISTE'
    end
@@ -34,32 +34,33 @@ else
 O valor deve ser retornado e armazenado em uma variável!! */
 
 create proc MINMAX
-( @TipoID int, --  se o usuário quer o menor ou a maior ID
-  @Tabela varchar(1) --  se o usuário quer selecionar a tabela pagar ou receber
+( @TipoID int, 
+  @Tabela varchar(1)
 )
 as
-  if @TipoID=0 -- o usuário que o menor ID
+  if @TipoID=0 /*se for 0 será o menor id*/
     begin
-       if @tabela='P' -- o usuário que o menor ID do conta a pagar        
+       if @tabela='P' /*se for o menor id e for da tabela pagar*/     
           select min(idpagar) as minimo from pagar
-       else --se @tabela for diferente de P o usuário quer o menor ID do receber
+       else /*se for o menor id da tabela receber*/
           select min(idreceber) as minimo from receber
      end
-  else  -- se  @tipoID nÃo for 0 é pq o usuário que o MAIOR id
+  else  /*se não for o menor id, será o maior id*/
      begin
-       if @tabela='P' -- o usuário que o menor ID do conta a pagar        
+       if @tabela='P' /*se for o maior id e for da tabela pagar*/           
           select MAX(idpagar) as maximo from pagar
-       else --se @tabela for diferente de P o usuário quer o menor ID do receber
+       else /*se não for o menor id, será o maior id*/
           select MAX(idreceber) as maximo from receber
      end
 
 /* 3) Store procedure para gerar um extrato de clientes em um período desejado. 
-O usuário deve passar o NOME do cliente desejado , 
-o período desejado, 
-e informar de deseja ver somente as faturas RECEBIDAS (0), somente as faturas A RECEBER (1) ou Todas as faturas (2). 
+O usuário deve passar o NOME do cliente desejado, o período desejado, e informar de deseja ver somente:
+as faturas RECEBIDAS (0), 
+somente as faturas A RECEBER (1), 
+ou Todas as faturas (2). 
 
 A store procedure  deve retornar os seguintes dados:
-Empresa	Descrição	Valor Data vencimento	Data Pagamento
+Empresa, Descrição, Valor, Data vencimento, Data Pagamento
 */
 
 create proc Extrato
@@ -70,19 +71,19 @@ create proc Extrato
 )
 as	
 
-if @tipodesejado=0 -- somente faturas já recebidas
+if @tipodesejado=0 -- se for 0 será as faturas recebidas
    begin
        select emp_razaosocial as empresa, rec_descricao as descricao,
            rec_vlrpagto as valor, rec_dtvencimento as vencimento,
            rec_dtpagto as DataPagamento
-       from empresa,receber
+       from empresa, receber
        where fkempresa=idempresa
-         and emp_razaosocial like @nome+'%' -- Não esquecer do sinal %,               	   and rec_dtvencimento>=@dataInicial  ---senão o sql ira mostra
-         and rec_dtvencimento<=@dataFinal    -- somente os clientes 
-         and rec_dtpagto is not null 	    -- EXATAMENTE igual ao 								    --	nome digitado
+         and emp_razaosocial like @nome+'%' -- Não esquecer do sinal %, senão o sql ira mostrar somente os clientes EXATAMENTE igual ao nome digitado
+         and rec_dtvencimento<=@dataFinal 
+         and rec_dtpagto is not null
     end
 
-if @tipodesejado=1 -- somente faturas NÃO recebidas
+if @tipodesejado=1 -- se for 1 será as faturas NÃO recebidas
    begin
        select emp_razaosocial as empresa, rec_descricao as descricao,
            rec_vlrpagto as valor, rec_dtvencimento as vencimento,
@@ -92,9 +93,7 @@ if @tipodesejado=1 -- somente faturas NÃO recebidas
          and emp_razaosocial like @nome+'%'
          and rec_dtvencimento>=@dataInicial
          and rec_dtvencimento<=@dataFinal
-	   	 and rec_dtpagto is null  -- so muda esta linha, is null 							   ---indica que o filtro vai pegar 						-- somente faturas com a data de 
-						-- pagto	em branco, ou seja, não
-						-- recebidas
+	   	 and rec_dtpagto is null  -- so muda esta linha, is null, indica que o filtro vai pegar somente faturas com a data de pagto	em branco, ou seja, não recebidas
     end
 
 if @tipodesejado=2 -- Todas as faturas
@@ -109,10 +108,15 @@ if @tipodesejado=2 -- Todas as faturas
          and rec_dtvencimento<=@dataFinal
     end
 
-/* 4) Store procedure para gerar um extrato resumido de todos os clientes em um determinado período. O usuário deve passar como paramento o período desejado e se deseja ver somente as faturar RECEBIDAS (0), somente as faturas A RECEBER (1) ou Todas as faturas (2).	A store procedure  deve retornar os seguintes dados:
+/* 4) Store procedure para gerar um extrato resumido de todos os clientes em um determinado período. 
+O usuário deve passar como paramento o período desejado e se deseja ver somente:
+as faturas RECEBIDAS (0), 
+somente as faturas A RECEBER (1) ou 
+Todas as faturas (2).	A store procedure  deve retornar os seguintes dados:
 
-	Empresa	Total Faturado	Total Já Recebido	Total a Receber
+Empresa	Total Faturado Total Já Recebido	Total a Receber
 */ 
+	
 create proc Extrato
 ( @tipodesejado	int,
   @datainicial	datetime,
@@ -134,11 +138,6 @@ if @tipodesejado=0 -- somente faturas já recebidas
        group by emp_razaosocial
     end
 
---- ATENÇÃO paras a linha:
---           sum(case when rec_dtpagto IS NULL then rec_vlrpagto else --  0 end)  as 'Valor A receber',
--- Antes de fazer o SUM o sql ira testar o campo rec_dtpagto, se este -- campo FOR NULO (ou seja, a fatura não esta paga)
--- ele soma o valor do campo no total a receber, senão soma zero. 
-
 if @tipodesejado=1 -- somente faturas NÃO recebidas
    begin
        select emp_razaosocial as empresa,
@@ -153,7 +152,6 @@ if @tipodesejado=1 -- somente faturas NÃO recebidas
        group by emp_razaosocial
     end
 
-
 if @tipodesejado=2 -- Todas as faturas
    begin
        select emp_razaosocial as empresa,
@@ -166,4 +164,3 @@ if @tipodesejado=2 -- Todas as faturas
          and rec_dtvencimento<=@dataFinal
        group by emp_razaosocial
     end
-
