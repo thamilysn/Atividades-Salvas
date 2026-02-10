d04e605ce8a59b4d40a7d8fb3b2a2909c9d2f3e4
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-02-10 08:37:44 -0300

    Create 10.02 - Começo.c

---
+++ Estrutura de Dados/10.02 - Começo.c
@@ -0,0 +1,16 @@
+int main()
+{
+    printf("\tMENU: Escolha uma das opções\n");
+    printf("\tOpcao 1: Adicionar\n");    
+    printf("\tOpcao 2: Subtrair\n");
+    printf("\tOpcao 3: Multiplicar\n");
+    printf("\tOpcao 4: Dividir\n");
+    printf("\tOpcao 0: Sair do programa\n");
+
+    printf("\tDigite a opcao desejada (apenas numeros):\n");
+    
+    int x;
+    scanf("%d", &x);
+    printf("%d", x);
+    return 0;
+}
