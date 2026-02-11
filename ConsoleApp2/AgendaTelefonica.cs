f11bb4706e27cd9da81978966801156cac5d1d92
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-02-11 08:51:27 -0300

    Add files via upload

---
+++ ConsoleApp2/AgendaTelefonica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class AgendaTelefonica
+    {
+        Dictionary<String, String> dictionary = new();
+
+        public void inserir(String nome, String numero)
+        {
+            dictionary.Add(nome, numero);
+        }
+
+        public string buscarNumero(String nome)
+        {
+            if (dictionary.ContainsKey(nome))
+                return dictionary[nome];
+            else
+                return "Contato não encontrado.";
+            }
+
+        public void remover(String nome)
+        {
+            dictionary.Remove(nome);
+        }
+
+        public int tamanho()
+        {
+
+        }
+    }
+}
