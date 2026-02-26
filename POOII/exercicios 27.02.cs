82851baf3d26967fa8a072b6adcd98c633d333e5
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-02-26 20:45:02 -0300

    Update exercicios 27.02.cs

--- POOII/exercicios 27.02.cs
+++ POOII/exercicios 27.02.cs
@@ -1,1 +1,46 @@
+//Exercicio 1 - UVV Acadêmico
+//Classe professor
+public abstract class Professor
+{
+    protected string nome;
+    protected int matricula;
+    protected int cargaHoraria;
+    protected double beneficio;
 
+    public Professor(string nome, int matricula, int cargaHoraria)
+    {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.cargaHoraria = cargaHoraria;
+        this.beneficio = 0;
+    }
+
+    public abstract void calcularBeneficio();
+
+    public double getBeneficio()
+    {
+        return beneficio;
+    }
+
+    public override string ToString()
+    {
+        return $"Nome: {nome} | Matrícula: {matricula} | Carga Horária: {cargaHoraria}h | Benefício: R$ {beneficio:F2}";
+    }
+}
+
+public class ProfDE : Professor
+{
+    public ProfDE(string nome, int matricula, int cargaHoraria)
+        : base(nome, matricula, cargaHoraria)
+    {
+    }
+
+    public override void calcularBeneficio()
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"[ProfDE] {base.ToString()}";
+    }
+}
