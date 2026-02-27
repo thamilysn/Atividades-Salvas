fc80640575e0dcf8706ece576d3448772aebe5ab
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-02-27 08:44:30 -0300

    Add files via upload

---
+++ POOII/Atividade 27.03/Exercicio2/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercicio2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirJanela_Click(object sender, RoutedEventArgs e)
+        {
+            string nome = "Thamily";
+
+            Janela2 janela = new Janela2(nome);
+            janela.Show();
+        }
+    }
+}
