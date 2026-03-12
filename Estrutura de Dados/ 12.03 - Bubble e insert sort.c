4b223f72ced342bf0d53b108e6bea535e04ef6ce
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-03-12 10:03:15 -0300

    Update  12.03 - Bubble e insert sort.c

--- Estrutura de Dados/ 12.03 - Bubble e insert sort.c
+++ Estrutura de Dados/ 12.03 - Bubble e insert sort.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 void bubbleSort(int *v, int x){
-    int temp;
+    int y;
     for(int i = 0; i < x - 1; i++){
         for(int j = 0; j < x - i - 1; j++){
             if(v[j] > v[j + 1]){
-                temp = v[j];
+                y = v[j];
                 v[j] = v[j + 1];
-                v[j + 1] = temp;
+                v[j + 1] = y;
             }
         }
     }
@@ -19,6 +19,36 @@ int main()
     int v[7] = {1, 3, 5, 8, 9, 2, 6};
 
     bubbleSort(v, 7);
+    for(int i = 0; i < 7; i++){
+        printf("%d|", v[i]);
+    }
+    return 0;
+}
+
+//InsertionSort
+#include <stdio.h>
+
+void insertSort(int *v, int x){
+    int i;
+    int j;
+    int y;
+    for(i = 1; i < x; i++){
+        y = v[i];
+        j = i - 1;
+
+        while(j >= 0 && v[j] > y){
+            v[j + 1] = v[j];
+            j--;
+        }
+
+        v[j + 1] = y;
+    }
+}
+
+int main(){
+    int v[7] = {1, 3, 5, 8, 9, 2, 6};
+
+    insertSort(v, 7);
 
     for(int i = 0; i < 7; i++){
         printf("%d|", v[i]);
@@ -26,5 +56,3 @@ int main()
 
     return 0;
 }
-
-//InsertionSort
