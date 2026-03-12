52f528ee43cf1403f3fbbcc1e06398d2856fdfd2
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-03-12 09:49:34 -0300

    Update 10.03 - SelectionSort.c

--- Estrutura de Dados/10.03 - SelectionSort.c
+++ Estrutura de Dados/10.03 - SelectionSort.c
@@ -1,11 +1,10 @@
 //SelectionSort
 #include <stdio.h>
 
-int SelectionSort(int *v, int x){
+void bubbleSort(int *v, int x){
     int y;
-    
     for(int i = 0; i < x; i++){
-        for(int j = 0; j < x; j++){
+        for(int j=0; j < x; j++){
             if(v[i] > v[j]){
                 y = v[i];
                 v[i] = v[j];
@@ -17,10 +16,10 @@ int SelectionSort(int *v, int x){
 
 int main()
 {
-    int v[7] = {7, 4, 5, 9, 8, 2, 1};
-    SelectionSort(v, 7);
+    int v[7] = {1, 3, 5, 8, 9, 2, 6};
+    bubbleSort(v, 7);
     for(int i = 0; i < 7; i++)
-    printf("Os valores são: %d \n", v[i]);
+    printf("%d|", v[i]);
 
     return 0;
 }
