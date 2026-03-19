8a000d42817911947789f62e279c0ed0c4db2dd9
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-03-19 10:42:33 -0300

    Implement MergeSort algorithm in C

--- Estrutura de Dados/19.03 - MergeSort e QuickSort.c
+++ Estrutura de Dados/19.03 - MergeSort e QuickSort.c
@@ -21,4 +21,56 @@ int main() {
 }
 
 //MergeSort
+#include <stdio.h>
+
+void mergeSort(int *V, int inicio, int fim){
+    int meio;
+    if(inicio < fim){
+        meio = floor((inicio+fim) / 2);
+        
+        mergeSort (V, inicio, meio);
+        mergeSort (V, meio + 1, fim);
+        mergeSort (V, inicio, meio, fim);
+    }
+}
+
+void merge(int *V, int inicio, int meio, int fim){
+    int i = inicio;
+    int j = meio + 1;
+    int k = 0;
+
+    int *aux = (int*) malloc((fim - inicio + 1) * sizeof(int));
+
+    while (i <= meio && j <= fim){
+        if (V[i] < V[j])
+            aux[k++] = V[i++];
+        else
+            aux[k++] = V[j++];
+    }
+
+    while (i <= meio){
+        aux[k++] = V[i++];
+    }
+
+    while (j <= fim){
+        aux[k++] = V[j++];
+    }
+
+    for (i = inicio, k = 0; i <= fim; i++, k++){
+        V[i] = aux[k];
+    }
+
+    free(aux);
+}
+
+int main()
+{
+    int V[10] = {12, 15, 43, 10, 28, 45, 19, 4, 16, 13};
+    mergeSort(V, 10);
+    for(int i = 0; i < 10; i++)
+    printf("%d|", V[i]);
+
+    return 0;
+}
+
 //QuickSort
