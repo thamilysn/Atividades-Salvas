f06f52b4a4bea8170098908a7dac834a9c6293bf
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-04-07 08:27:06 -0300

    Create Lista Encadeada Cont.c

---
+++ Estrutura de Dados/Lista Encadeada Cont.c
@@ -0,0 +1,86 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cel{
+    int conteudo;
+    struct cel *seg;
+}cel;
+
+typedef struct cel* Lista;
+
+Lista* cria_lista(){
+    Lista *li = (Lista*) malloc (sizeof(Lista));
+    if(li != NULL){
+        *li = NULL;
+    }
+    return li;
+}
+
+int insere_lista_fim(Lista *lista, int x){
+    if(lista == NULL){ return 0; }
+    
+    cel* aux = (cel*) malloc (sizeof(cel));
+    if(aux == NULL){ return 0; }
+    
+    aux -> conteudo = x;
+    aux -> seg = NULL;
+    
+    if((*lista) == NULL){
+        *lista = aux;
+    }else{
+        cel *temp;
+        temp = *lista;
+        while(temp -> seg != NULL){
+            temp = temp -> seg;
+        }
+        temp -> seg = aux;
+    }
+    return 1;
+}
+
+void imprimir_lista(Lista* lista){
+    if(lista == NULL){
+        printf("Lista não existe \n");
+    }
+    else{
+        if(*lista == NULL){
+            printf("\nLista Vazia\n");
+        }else{
+            
+            cel *temp;
+            temp = *lista;
+            printf("\n Lista: ");
+            
+            while(temp != NULL){
+                printf("%d |", temp -> conteudo);
+                temp = temp -> seg;
+            }
+            printf("\n");
+        }
+    }
+}
+
+int main()
+{
+    printf("Inicio\n");
+
+    Lista *lst;
+    lst = cria_lista();
+    
+    insere_lista_fim(lst, 1);
+    insere_lista_fim(lst, 2);
+    insere_lista_fim(lst, 3);
+    insere_lista_fim(lst, 4);
+    insere_lista_fim(lst, 5);
+    
+    imprimir_lista(lst);
+    return 0;
+}
