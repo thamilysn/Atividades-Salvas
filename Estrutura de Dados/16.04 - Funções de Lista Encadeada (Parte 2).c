fa508985ff23d07863e8393b98502b51c48c2aed
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-04-16 10:14:17 -0300

    Update 16.04 - Funções de Lista Encadeada (Parte 2).c

--- Estrutura de Dados/16.04 - Funções de Lista Encadeada (Parte 2).c
+++ Estrutura de Dados/16.04 - Funções de Lista Encadeada (Parte 2).c
@@ -1,74 +1,108 @@
 //codigo pra liberar lista de tras pra frente
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 typedef struct cel{
-  int conteudo;
-  struct cel *seg;
+    int conteudo;
+    struct cel *seg;
 }cel;
 
 typedef struct cel* Lista;
 
-void imprime_lista(Lista* lista){
-  if(lista==NULL){
-    return;
-  }
-  cel* aux = *lista;
-  while(aux!=NULL){
-    printf("%d -> ", aux->conteudo);
-    aux=aux->seg;
-  }
-  printf("NULL\n");
-}
-
 Lista* cria_lista(){
-  Lista *li = (Lista*) malloc(sizeof(Lista));
-  if(li != NULL){
-    *li=NULL;
-  }
-  return li;
-}
-
-int insere_lista_fim(Lista* lista, int x){
-  if(lista==NULL) {return 0;}
-  cel* aux = (cel*) malloc(sizeof(cel));
-  if(aux==NULL){return 0;}
-  aux->conteudo = x;
-  aux->seg = NULL;
-  if((*lista)==NULL){ *lista = aux;}
-  else{
-    cel *temp;
-    temp = *lista;
-    while(temp->seg!= NULL){ //caminha até o utlimo elemento
-      temp= temp->seg;
+    Lista *li = (Lista*) malloc (sizeof(Lista));
+    if(li != NULL){
+        *li = NULL;
     }
-    temp->seg = aux;
-  }
-  return 1;
+    return li;
 }
 
-int insere_lista_inicio(Lista* lista, int x) {
-  if (lista == NULL) { return 0; }
-
-  cel* aux = (cel*) malloc(sizeof(cel));
-  if (aux == NULL) { return 0; }
-
-  aux->conteudo = x;
-  aux->seg = *lista;
-
-  *lista = aux;
-  return 1;
-
-}
-
-int busca(Lista *lista, int valor){
-  cel *p;
-  for(p=*lista;p!=NULL;p=p->seg){
-    if(p->conteudo == valor){
-      return 1;
+int insere_lista_fim(Lista *lista, int x){
+    if(lista == NULL){ return 0; }
+    
+    cel* aux = (cel*) malloc (sizeof(cel));
+    if(aux == NULL){ return 0; }
+    
+    aux -> conteudo = x;
+    aux -> seg = NULL;
+    
+    if((*lista) == NULL){
+        *lista = aux;
+    }else{
+        cel *temp;
+        temp = *lista;
+        while(temp -> seg != NULL){
+            temp = temp -> seg;
+        }
+        temp -> seg = aux;
     }
-  }
-  return 0;
+    return 1;
+}
+
+int insere_lista_inicio(Lista *lista, int x){
+    if(lista == NULL){ return 0; }
+    
+    cel* aux = (cel*) malloc (sizeof(cel));
+    if(aux == NULL){ return 0; }
+    
+    aux -> conteudo = x;
+    aux -> seg = *lista;
+    
+    *lista = aux;
+    return 1;
+}
+
+void imprimir_lista(Lista* lista){
+    if(lista == NULL){
+        printf("Lista não existe \n");
+    }
+    else{
+        if(*lista == NULL){
+            printf("\nLista Vazia\n");
+        }else{
+            
+            cel *temp;
+            temp = *lista;
+            printf("\nLista: ");
+            
+            while(temp != NULL){
+                printf("%d |", temp -> conteudo);
+                temp = temp -> seg;
+            }
+            printf("\n");
+        }
+    }
+}
+
+int busca_while(Lista *lista, int x){
+    if(lista == NULL) { return 0; }
+    if(*lista == NULL) { return 0; }
+    
+    cel *aux = *lista; 
+    
+    while(aux != NULL){
+        if(aux->conteudo == x){
+            return 1; //se encontrar, é 1
+        }
+        aux = aux->seg;
+    }
+    
+    return 0; //se não, retorna 0
+}
+
+int contar_lista(Lista *lista){
+    if(lista == NULL) { return 0; }
+    if(*lista == NULL) { return 0; }
+
+    int cont = 0;
+    cel *aux = *lista;
+
+    while(aux != NULL){
+        cont++;
+        aux = aux->seg;
+    }
+
+    return cont;
 }
 
 void libera_lista(Lista *lista){
@@ -107,22 +141,28 @@ int remove_lista(Lista *lista, int x){
     return 1;
 }
 
-int main(void) {
-  Lista *lst;
-  lst = cria_lista();
-  insere_lista_fim(lst, 1);
-  insere_lista_fim(lst, 2);
-  insere_lista_fim(lst, 3);
-  insere_lista_fim(lst, 4);
-  insere_lista_fim(lst, 5);
-  imprime_lista(lst);
+int main()
+{
+    printf("Inicio: \n");
 
-  remove_lista(lst,4);
-  imprime_lista(lst);
-  printf("\nA busca retornou %d\n",busca(lst,7));
-  
-  libera_lista(lst);
-  imprime_lista(lst);
-
+    Lista *lst;
+    lst = cria_lista();
+    
+    insere_lista_fim(lst, 1);
+    insere_lista_fim(lst, 2);
+    insere_lista_fim(lst, 3);
+    insere_lista_fim(lst, 4);
+    insere_lista_fim(lst, 5);
+    
+    insere_lista_inicio(lst, 0);
+    imprimir_lista(lst);
+    
+    printf("%d", busca_while(lst, 0));
+    printf("\nTotal de elementos: %d\n", contar_lista(lst));
+    
+    return 0;
+}
   return 0;
 }
+
+//implementar as alterações necessárias em cada uma das funções para que ela seja completamente encadeada
