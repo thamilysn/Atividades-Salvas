da932c84a10c2eab4cd5621ccbe563b0e9b07d04
Thamily Silva Nogueira <thamilysilvanogueira@gmail.com>
2026-08-03 10:06:01 -0300

    Create Exercícios-03.08.c

---
+++ Estrutura de Dados II/Exercícios-03.08.c
@@ -0,0 +1,23 @@
+//1)Sabendo que:
+//f(1) = 1
+//f(2) = 1
+//f(n) = f(n-2) + f(n-1) V n >= 2
+//prove que f(n+4) = 3f(n+2) - f(N) V N >= 1
+
+//2) Define uma operação recorrente para a multiplicação de dois inteiros positivos.
+
+//3) Implemente em C a operação definida em 2).
+
+//4) Implemente em C uma função recursiva que retorna o maior elemento de um vetor de tamanho n.
+
+//5) Recursão de Ackemann: A(m, n) = {n+1, se m = 0; 
+//                                   A(m-1), se m > 0 e n = 0; 
+//                                   A(m-1, A(m, n-1), se m > 0 e n >0}
+//Calcule:
+//a) A(0,n)
+//b) A(1,n)
+//c) A(2,n)
+//d) A(3,n)
+//e) A(2,1)
+
+//6) 
